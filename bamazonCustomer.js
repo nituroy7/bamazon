71c503aaf5ef2b766e4eb2342468e9382ce2ae81
nituroy7 <nitu_prodhani@yahoo.com>
2017-07-24 00:09:27 -0500

    add

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -16,68 +16,89 @@ var connection = mysql.createConnection({
 
 connection.connect(function(err) {
     if (err) throw err;
-    console.log("connected as id " + connection.threadId + "\n");
 
     connection.query("SELECT * FROM bamazon.products", function(err, res) {
         if (err) throw err;
 
         for (var i = 0; i < res.length; i++) {
-            console.log("\n" + res[i].item_id + " | " + res[i].product_name + " | " + res[i].department_name + " | " + res[i].price +
-                res[i].stock_quantity);
+            console.log("\n\n" + "ID : " + res[i].item_id + "\n--------" + "\n\n" + "Product Name: " +
+                res[i].product_name + " \n\n" + "Department : " + res[i].department_name + "\n\n" +
+                "Price: " + "$" + res[i].price + "\n");
+
         }
-        console.log("-----------------------------------");
 
         item_Search();
 
     });
 });
 
-// The app should then prompt users with two messages.
-// The first should ask them the ID of the product they would like to buy.
-
-
 function item_Search() {
     inquirer
         .prompt({
             name: "item_id",
             type: "input",
-            message: "What is the ID of the product you would like to buy?",
+            message: "\n What is the ID of the product you would like to buy?",
         })
         .then(function(answer) {
             var query = "SELECT item_id, product_name, department_name, price, stock_quantity FROM products WHERE ?";
             connection.query(query, { item_id: answer.item_id }, function(err, res) {
 
-                for (var i = 0; i < res.length; i++) {
-                    console.log("\n" + res[i].item_id + " | " + res[i].product_name + " | " + res[i].department_name + " | " + res[i].price + " | " +
-                        res[i].stock_quantity);
-                }
-
+                // for (var i = 0; i < res.length; i++) {
+                //     console.log("\n" + res[0].item_id + " | " + res[0].product_name + " | " + res[0].department_name + " | " + "$" + res[0].price);
+                // }
+                item_Buy(answer);
             });
+
         });
+
 };
 
+function item_Buy(item) {
+    inquirer
+        .prompt({
+            name: "stock_quantity",
+            type: "input",
+            message: "\n How many units of the product you would like to buy?",
+        })
+        .then(function(answer) {
 
-//The second message should ask how many units of the product they would like to buy.
-// Once the customer has placed the order, your application should check if your store has enough of the product to meet the customer's request.
-// If not, the app should log a phrase like Insufficient quantity!, and then prevent the order from going through.
-// However, if your store does have enough of the product, you should fulfill the customer's order.
+            var query = "SELECT item_id, product_name, department_name, price, stock_quantity FROM products WHERE ?";
+            connection.query(query, { item_id: item.item_id }, function(err, res) {
 
-// function item_Buy() {
-//     inquirer
-//         .prompt({
-//             name: "stock_quantity",
-//             type: "input",
-//             message: "How many units of the product you would like to buy?",
-//         })
-//         .then(function(answer) {
-//             var query = "SELECT item_id, product_name, department_name, price, stock_quantity FROM products WHERE ?";
-//             connection.query(query, { item_id: answer.item_id }, function(err, res) {
+                console.log("\n Available quantity: " + res[0].stock_quantity);
 
-//                 for (var i = 0; i < res.length; i++) {
-//                     console.log("\n" + res[i].item_id + " | " + res[i].product_name + " | " + res[i].department_name + " | " + res[i].price + " | " +
-//                         res[i].stock_quantity);
-//                 }
+                if (res[0].stock_quantity > answer.stock_quantity) {
 
-//             });
-//         });
-// };
+                    res[0].stock_quantity = res[0].stock_quantity - answer.stock_quantity;
+
+                    updateProduct(res[0].stock_quantity, item.item_id);
+
+                } else {
+                    console.log("\n Insufficient quantity!");
+                }
+                var cost = res[0].price * answer.stock_quantity;
+                console.log("\n " + answer.stock_quantity  + " products added to the Cart!" + "\n\n Total Cost: " + cost);
+
+            });
+
+        });
+
+};
+
+function updateProduct(stock, id) {
+
+    var query = connection.query(
+        "UPDATE products SET ? WHERE ?", [{
+                stock_quantity: stock
+            },
+            {
+                item_id: id
+            }
+        ],
+        function(err, res) {
+            // console.log("\n " + res.affectedRows + " products added to the Cart!");
+            console.log('\n Remaining quantity: ' + stock);
+        }
+    );
+};
+
